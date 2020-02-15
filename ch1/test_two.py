6ece8bf084231f7dab52c0a8b05c0bbc64f439fe
mizuhosi <shin1008@gmail.com>
2020-02-15 21:41:43 +0900

    fix pytest fail

--- ch1/test_two.py
+++ ch1/test_two.py
@@ -1,4 +1,4 @@
 def test_failing():
-    print('Hello Fail!')
-    assert (1, 2, 3) == (3, 2, 1)
-    # assert (1, 2, 3) == (1, 2, 3)
+    # print('Hello Fail!')
+    # assert (1, 2, 3) == (3, 2, 1)
+    assert (1, 2, 3) == (1, 2, 3)
