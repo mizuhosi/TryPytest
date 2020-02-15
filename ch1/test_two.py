57dea70e2e7042577138d01178560d9f98364aea
mizuhosi <shin1008@gmail.com>
2020-02-15 21:37:21 +0900

    fix flake8 message

--- ch1/test_two.py
+++ ch1/test_two.py
@@ -1,5 +1,4 @@
 def test_failing():
-    # assert (1, 2, 3) == (3, 2, 1)
-    assert (1, 2, 3) == (1, 2, 3)
-    
-
+    print('Hello Fail!')
+    assert (1, 2, 3) == (3, 2, 1)
+    # assert (1, 2, 3) == (1, 2, 3)
